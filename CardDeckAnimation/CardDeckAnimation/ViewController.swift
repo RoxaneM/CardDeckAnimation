3950c9e19a907a27b27663814908e666b83582c2
Roxane Gud <roxane654@gmail.com>
2018-11-11 15:39:06 +0200

    Project created

---
+++ CardDeckAnimation/CardDeckAnimation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CardDeckAnimation
+//
+//  Created by Roxane Gud on 11/11/18.
+//  Copyright © 2018 Roxane Markhyvka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ CardDeckAnimation/CardDeckAnimationTests/CardDeckAnimationTests.swift
@@ -0,0 +1,34 @@
+//
+//  CardDeckAnimationTests.swift
+//  CardDeckAnimationTests
+//
+//  Created by Roxane Gud on 11/11/18.
+//  Copyright © 2018 Roxane Markhyvka. All rights reserved.
+//
+
+import XCTest
+@testable import CardDeckAnimation
+
+class CardDeckAnimationTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
